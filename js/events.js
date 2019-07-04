60276a4d0c3433985ed22a353aea73a03bfabe7a
Richard Pitts <richardepitts@gmail.com>
2019-07-04 01:12:31 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,25 +3,35 @@
 $(document).ready(function(){
 
 
-function getIt() {
-$('p').on('click', function() {
-  window.alert("Hey!");
-});
-}
-
-
-
-function frameIt() {
-  $('img').on("load", function() {
-    img.addClass("tasty")
-  })
-}
-
-function pressIt() {
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
   
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
 }
 
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
 
-})
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 
